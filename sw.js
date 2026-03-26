b104bc0093b3cdf4f0bf674c82d49b503648e1bb
KyleZebra <kay.cepera@googlemail.com>
2026-03-26 15:49:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'regen-tracker-v7.3';
+const CACHE_NAME = 'regen-tracker-v7.4';
 const urlsToCache = [
   './',
   './index.html',
@@ -11,6 +11,28 @@ self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
+// WICHTIG: Die neue Activate-Phase räumt sofort gnadenlos alte Caches ab
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+// Lauscht auf den Befehl "skipWaiting" aus unserer index.html
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
