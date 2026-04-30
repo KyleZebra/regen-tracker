78992829c6e8e64bc37cefc0dedcfa42173ee271
KyleZebra <kay.cepera@googlemail.com>
2026-04-30 10:28:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // sw.js - Service Worker & Offline Cache
 // ==========================================
 
-const CACHE_NAME = 'retrack-cache-v18.3';
+const CACHE_NAME = 'retrack-cache-v18.31';
 
 const urlsToCache = [
     './',
