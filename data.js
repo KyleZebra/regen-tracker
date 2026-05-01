67838cc55c55e8fb34ef7ad896076a6d2c6fc5aa
KyleZebra <kay.cepera@googlemail.com>
2026-05-01 10:14:07 +0200

    Aktualisieren von data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 // data.js - State Management & Core Helpers
 // ==========================================
 
-const APP_VERSION = '18.5 (OffByOne)';
+const APP_VERSION = '18.6 (OffByOne)';
 
 // --- Defensive DOM Helpers ---
 const safeVal = (id) => { 
