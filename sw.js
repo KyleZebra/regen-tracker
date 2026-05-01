790be479329d30e0139ffe472ef752f6167d72bd
KyleZebra <kay.cepera@googlemail.com>
2026-05-01 09:56:45 +0200

    Aktualisieren von sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // sw.js - Service Worker & Offline Cache
 // ==========================================
 
-const CACHE_NAME = 'retrack-cache-v18.4';
+const CACHE_NAME = 'retrack-cache-v18.5';
 
 const urlsToCache = [
     './',
